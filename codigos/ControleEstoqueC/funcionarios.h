acf74bababb6e1454d28240fabb2e9374e343173
brunoqualhato <brunoqualhato1@gmail.com>
2017-10-30 22:30:39 -0200

    grupo funcionarios

---
+++ codigos/ControleEstoqueC/funcionarios.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   funcionarios.h
+ * Author: aluno
+ *
+ * Created on 30 de Outubro de 2017, 22:11
+ */
+
+#ifndef FUNCIONARIOS_H
+#define FUNCIONARIOS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FUNCIONARIOS_H */
+

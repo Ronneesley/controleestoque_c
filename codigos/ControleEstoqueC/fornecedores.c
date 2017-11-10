ecd8c7e863e2ebcf2cb9ad36a050c21db87455a5
wellcerv <wellcerv@hotmail.com>
2017-11-10 15:05:53 -0200

    Alteracao Fornecedores

--- codigos/ControleEstoqueC/fornecedores.c
+++ codigos/ControleEstoqueC/fornecedores.c
@@ -1,5 +1,14 @@
 #include"fornecedores.h"
 
+//Borda Padrão dos Menus
+void BordaPadrao(){
+    printf("|--------------------------------------------------------------------|\n");
+}
+
+//Pausar uma Funcao
+void pause(){
+    system("pause");
+}
 
 // Menu Principal do Cadastro de Fornecedores.
 void MenuFornecedores(){
@@ -9,14 +18,14 @@ void MenuFornecedores(){
     do{
                 
         limparTela();
-        printf("|--------------------------------------------------------------------|\n");
+        BordaPadrao();
         printf("|\t\t\tO que deseja fazer?\t\t\t     |\n");
-        printf("|--------------------------------------------------------------------|\n");
+        BordaPadrao();
         printf("\n");
-        printf("1) Consultar o Cadastro Fornecedores\n");
-        printf("2) Cadastrar um novo país\n");
-        printf("3) Alterar um país\n");
-        printf("4) Excluir um país\n");
+        printf("1) Consultar Fornecedores\n");
+        printf("2) Cadastrar um Novo Fornecedor\n");
+        printf("3) Alterar um Fornecedor\n");
+        printf("4) Excluir um Fornecedor\n");
         printf("5) Voltar ao menu principal\n\n");
 
         printf("Digite a opção desejada: ");
@@ -34,15 +43,46 @@ void MenuFornecedores(){
 
 void ConsultaFornecedores(){
     
-          
     limparTela();
-    printf("|--------------------------------------------------------------------|\n");
-    printf("| CADASTRO DE FORNECEDORES                                           |\n");
-    printf("|--------------------------------------------------------------------|\n");
-                    
-    
-    
+    BordaPadrao();
+    printf("| LISTAGEM DE FORNECEDORES                                           |\n");
+    BordaPadrao();
+    BordaPadrao();
 
+    //Cria a variável de conexão com o MySQL
+    MYSQL mysql;
+    mysql_init(&mysql);
+
+    //Efetua a conexão
+    if (mysql_real_connect(&mysql, SERVIDOR_BD, USUARIO_BD, SENHA_BD, NOME_BD, PORTA_BD, NULL, 0)){
+        //Executa o comando de consulta
+        if (mysql_query(&mysql, "select id, nome from paises order by nome") == 0){
+            //Obtém o resultado
+            MYSQL_RES *resultado = mysql_store_result(&mysql);
+
+            //Cria uma variável para guardar a linha
+            MYSQL_ROW linha;
+            while ( (linha = mysql_fetch_row(resultado)) ){
+                //Obtém cada coluna na órdem
+                int id = atoi(linha[0]);
+                char *nome = linha[1];
+
+                //Imprime cada linha
+                printf("| %10d | %-53s |\n", id, nome);
+            }
+
+            //Libera os resultado e fecha a conexão
+            mysql_free_result(resultado);
+            mysql_close(&mysql);
+        } else {
+            printf("%s\n", mysql_error(&mysql)); //Exibe a mensagem de erro
+        }
+    } else {
+        printf("Falha ao conectar no banco de dados: %s\n", mysql_error(&mysql)); //Exibe a mensagem de erro ao conectar 
+    }
+
+printf("Pressione qualquer tecla para voltar ao Menu Anterior:");
+pause();
 
 
 }

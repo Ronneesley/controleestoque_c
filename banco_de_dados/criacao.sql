ab9a0f344e98d53b2c0edf5b352a1a569b2232c3
LuanaQ <31023070+LuanaQ@users.noreply.github.com>
2017-11-24 12:13:18 -0200

    Remoção de Tabela

--- banco_de_dados/criacao.sql
+++ banco_de_dados/criacao.sql
@@ -25,46 +25,4 @@ create table cidades(
 );
 
 
-create table fornecedor (
-	idFornecedor int not null auto_increment primary key,
-	nome varchar (100) not null,
-	endereco varchar(45) not null,
-	telefone varchar(15) not null,
-	cnpj varchar (45) not null,
-	insc_estadual int not null,
-	cep varchar(10) not null,
-	data_cadastro datetime not null,
-	idCidades int not null,
-	constraint foreign key (idCidades) references cidades(id)
-);
-
-create table pessoas(
-	idPessoas int not null auto_increment primary key,
-	nome varchar (100) not null,
-	sexo char(1) not null,
-	sexo char(1) not null,
-	data_nascimento date not null,
-	endereco varchar (150) not null,
-	cep varchar(10) not null,
-	telefone varchar(20) not null,
-	cpf varchar (20) not null,
-	rg varchar(20) not null
-);
-
-CREATE TABLE profissao(
-	  idProfissao int auto_increment primary key,
-	  profissao varchar(45) not null);
-
-create table funcionarios(
-	  idFuncionarios int auto_increment primary key,
-	   pis_pasep int (11) not null,
-	  fornecedor_idFornecedor INT NOT NULL,
-	  Pessoas_idPessoas INT NOT NULL,
-	  profissao_idprofissao INT NOT NULL,
-	  constraint foreign key (fornecedor_idFornecedor) references fornecedor(idFornecedor),
-	    constraint foreign key (Pessoas_idPessoas) references pessoas(idPessoas),
-	      constraint foreign key (profissao_idprofissao) references profissao(idProfissao)
-);
-  
-
 
